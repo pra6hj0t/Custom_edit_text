0014ea423fd0beaa5927d217ae551e7a6b253eb6
pra6hj0t <157087592+pra6hj0t@users.noreply.github.com>
2024-01-30 01:02:48 -0700

    data

---
+++ app/src/main/java/com/example/custom_edit_text/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.custom_edit_text;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+     Button btn_clr;
+     EditText edit_text;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//
+
+
+
+
+
+    }
+}
